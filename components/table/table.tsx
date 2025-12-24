cfce8f2c36c9f3bc28f5b7da49451d82f989f8ac
Venkatesh Amal Rajesh <venkatesh.amal@zentropytech.com>
2025-12-24 12:45:22 +0530

    final commit

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -8,7 +8,7 @@ type Props = TableProps & {
 
 export function Table({ columns, data, onRowClick }: Props) {
   return (
-    <div className="overflow-x-auto mx-1 lg:mx-20 dark:border-white border-black border-2 max-w-full lg:max-w-9xl bg-gray-200 dark:bg-gray-700 rounded-lg flex justify-center">
+    <div className="overflow-x-auto mx-1 lg:mx-20 dark:border-white border-black border-2 max-w-full lg:max-w-9xl bg-gray-200 dark:bg-gray-700 rounded-lg flex justify-between">
       <table className="w-full">
         <TableHeader columns={columns} />
         <tbody>
